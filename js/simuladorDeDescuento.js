d6b18abce0af90ca7edab2f8379c9450ae1053ad
JuanVillalva <juanpablovillalva2@gmail.com>
2025-03-24 03:12:02 -0300

    lol

--- js/simuladorDeDescuento.js
+++ js/simuladorDeDescuento.js
@@ -11,17 +11,16 @@ function cancelPurchase() {
 function getPrice() {
     const products = JSON.parse(localStorage.getItem("Remeras"));
 
-    // let units = 0;
-    // let price = 0;
-    // if (products && products.length > 0) {
-    //     products.forEach(product => {
-    //         units += product.amount;
-    //         price += product.price * product.amount;
-    //     });
-    // }
+    let units = 0;
+    let price = 0;
+    if (products && products.length > 0) {
+        products.forEach(product => {
+            units += product.amount;
+            price += product.price * product.amount;
+        });
+    }
 
-    upadteTotals();
 
-    alert(`Este es el precio total: $${upadteTotals()}.`)
+    alert(`Este es el precio total: $${price}.`)
     // return totalPrice;
 }
